9d1926a2d9069fb7df50c41ebd1a83f42a431ff5
Mohamed Yamani <myamani@192.168.0.101>
2023-04-24 02:04:49 +0100

    login

--- app/layout.tsx
+++ app/layout.tsx
@@ -4,6 +4,7 @@ import Navbar from "./components/navbar/Navbar"
 import ClientOnly from "./components/ClientOnly"
 import RegisterModal from "./components/modals/RegisterModal"
 import ToasterProvider from "./providers/ToasterProvider"
+import LoginModal from "./components/modals/LoginModal"
 
 export const metadata = {
   title: 'myamani',
@@ -27,6 +28,7 @@ export default function RootLayout({
 
         <ClientOnly>
           <ToasterProvider />
+          <LoginModal />
           <RegisterModal />
           <Navbar />
         </ClientOnly>
